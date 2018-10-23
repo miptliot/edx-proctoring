5416b42577caf470849a77a962218d171f79bf6b
dm-v <strannik772@gmail.com>
2018-10-23 17:52:45 +0300

    EDX-151 - Support redis as message broker in WA and edx-proctoring

--- edx_proctoring/notifications.py
+++ edx_proctoring/notifications.py
@@ -1,5 +1,11 @@
+import json
 import logging
 import time
+
+try:
+    import redis
+except ImportError:
+    redis = None
 
 from celery import Celery
 from kombu import Exchange
@@ -9,9 +15,16 @@ from django.conf import settings
 log = logging.getLogger(__name__)
 
 
+class BrokerType(object):
+    AMQP = 'amqp'
+    REDIS = 'redis'
+
+
 class ProctorNotificator(object):
     _celery_app = None
     _exchange = None
+    _redis_connection = None
+    _redis_default_queue_id = 1
 
     _exchange_name = 'edx.proctoring.event'
     _routing_key = 'edx.proctoring.event'
@@ -30,6 +43,15 @@ class ProctorNotificator(object):
 
         log.info('Publish notification: %s' % str(msg))
 
+        if settings.CELERY_BROKER_TRANSPORT == BrokerType.AMQP:
+            cls._send_to_amqp(msg)
+        elif settings.CELERY_BROKER_TRANSPORT == BrokerType.REDIS:
+            cls._send_to_redis(msg)
+        else:
+            raise Exception('Unknown broker type: %s' % str(settings.CELERY_BROKER_TRANSPORT))
+
+    @classmethod
+    def _send_to_amqp(cls, msg):
         celery_app = cls._get_celery_app()
         with celery_app.producer_or_acquire() as producer:
             producer.publish(msg,
@@ -59,3 +81,58 @@ class ProctorNotificator(object):
         if cls._exchange is None:
             cls._exchange = Exchange(cls._exchange_name, type='fanout', durable=True)
         return cls._exchange
+
+    @classmethod
+    def _get_redis_client(cls):
+        if cls._redis_connection is None:
+            if redis is None:
+                raise Exception('Redis extension is not installed')
+            connection_params = {
+                'host': settings.CELERY_BROKER_HOSTNAME
+            }
+            if ':' in settings.CELERY_BROKER_HOSTNAME:
+                hostname_list = settings.CELERY_BROKER_HOSTNAME.split(':')
+                connection_params['host'] = hostname_list[0]
+                connection_params['port'] = int(hostname_list[1])
+            if settings.CELERY_BROKER_PASSWORD:
+                connection_params['password'] = settings.CELERY_BROKER_PASSWORD
+            cls._redis_connection = redis.Redis(**connection_params)
+        return cls._redis_connection
+
+    @classmethod
+    def _get_redis_queues(cls):
+        queues_key = '%s.daemons' % cls._exchange_name
+        redis_conn = cls._get_redis_client()
+        queues_dict = redis_conn.hgetall(queues_key)
+        log.info('Queues in redis [key %s]: %s' % (queues_key, str(queues_dict)))
+        res = []
+        if queues_dict:
+            res = [int(v) for k, v in queues_dict.items()]
+        if cls._redis_default_queue_id not in res:
+            # always send message at least to the default queue
+            # additional check to avoid loss some messages
+            # in case if LMS is already started but notification daemon not yet
+            res.insert(0, cls._redis_default_queue_id)
+        return sorted(res)
+
+    @classmethod
+    def _send_to_redis(cls, msg):
+        max_retries = 5
+        current_attempt = 1
+
+        while True:
+            try:
+                redis_conn = cls._get_redis_client()
+                redis_queues = ['%s.%d' % (cls._exchange_name, int(queue_id))
+                                for queue_id in cls._get_redis_queues()]
+                log.info('Try to push message to the queues: %s' % str(redis_queues))
+                for queue_name in redis_queues:
+                    redis_conn.lpush(queue_name, json.dumps(msg))
+                return
+            except redis.ConnectionError:
+                if current_attempt > max_retries:
+                    raise
+                else:
+                    time.sleep(current_attempt * 2)
+                    current_attempt += 1
+
